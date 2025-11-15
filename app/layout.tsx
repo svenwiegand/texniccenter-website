5c2b9e38447311bced50f00602c71e7839339fe9
Sven Wiegand <source@sven-wiegand.de>
2025-11-15 17:11:35 +0100

    Replaced fav icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,16 +24,7 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       {
-        url: '/icon-light-32x32.png',
-        media: '(prefers-color-scheme: light)',
-      },
-      {
-        url: '/icon-dark-32x32.png',
-        media: '(prefers-color-scheme: dark)',
-      },
-      {
-        url: '/icon.svg',
-        type: 'image/svg+xml',
+        url: '/apple-icon.png',
       },
     ],
     apple: '/apple-icon.png',
